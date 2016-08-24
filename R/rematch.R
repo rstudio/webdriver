38b7249b3b7d9dca4dfe5926a064c8d2227e0c64
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-24 11:08:02 +0100

    empty_result is needed for rematch stuff

--- R/rematch.R
+++ R/rematch.R
@@ -65,3 +65,14 @@ match1 <- function(text1, match1) {
       )
   }
 }
+
+empty_result <- function(text, pattern, ...) {
+  match <- regexpr(pattern, text, perl = TRUE, ...)
+  num_groups <- length(attr(match, "capture.names"))
+  structure(
+    replicate(num_groups + 1, list(), simplify = FALSE),
+    names = c(attr(match, "capture.names"), ".match"),
+    row.names = integer(0),
+    class = "data.frame"
+  )
+}
