87e7e60ebe34f08d4065d9f9d950c9e2ccac0b15
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-29 11:35:53 +0100

    Try installing phantom for pkg checks, on CRAN & CIs

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,5 +3,6 @@ library(webdriver)
 
 if (Sys.getenv("NOT_CRAN", "") != "" ||
     Sys.getenv("CI", "") != "") {
+  if (is.null(webdriver:::find_phantom())) install_phantomjs()
   test_check("webdriver")
 }
