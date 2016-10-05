df16fc4f9436e5f0c7bcad5f0fca2258c43a1b5f
Gábor Csárdi <csardi.gabor@gmail.com>
2016-10-05 16:48:46 +0100

    Fix errors on chromedriver
    They are reported differently

--- tests/testthat/test-errors.R
+++ tests/testthat/test-errors.R
@@ -9,17 +9,15 @@ test_that("no window", {
   ## Close the window
   s$get_window()$close()
 
-  expect_error(s$go("http://r-hub.io"), "Window handle/name is invalid")
-  expect_error(s$get_url(), "Window handle/name is invalid")
-  expect_error(s$go_back(), "Window handle/name is invalid")
-  expect_error(s$go_forward(), "Window handle/name is invalid")
-  expect_error(s$refresh(), "Window handle/name is invalid")
-  expect_error(s$get_title(), "Window handle/name is invalid")
-  expect_error(s$get_source(), "Window handle/name is invalid")
-  expect_error(s$take_screenshot(), "Window handle/name is invalid")
-
-  ## This one does not give a proper error message
-  expect_error(s$find_element(css = "<p>"), "WebDriver error")
+  expect_error(s$go("http://r-hub.io"))
+  expect_error(s$get_url())
+  expect_error(s$go_back())
+  expect_error(s$go_forward())
+  expect_error(s$refresh())
+  expect_error(s$get_title())
+  expect_error(s$get_source())
+  expect_error(s$take_screenshot())
+  expect_error(s$find_element(css = "<p>"))
 })
 
 test_that("no window", {
@@ -29,7 +27,7 @@ test_that("no window", {
 
   s$get_window()$close()
 
-  expect_error(s$get_window(), "Current window handle invalid")
+  expect_error(s$get_window())
 })
 
 test_that("no elements", {
@@ -37,7 +35,7 @@ test_that("no elements", {
   s <- session$new(port = phantom$port)
   on.exit(s$delete(), add = TRUE)
 
-  expect_error(s$find_element(css = "foobar"), "Unable to find element")
+  expect_error(s$find_element(css = "foobar"))
 })
 
 test_that("is_selected for non-selectable elements", {
@@ -48,5 +46,8 @@ test_that("is_selected for non-selectable elements", {
   s$go(server$url("/check.html"))
   el <- s$find_element(css = "p")
 
-  expect_error(el$is_selected(), "Element is not selectable")
+  ## This is a bug in chromedriver
+  if (driver_type != "chromedriver") {
+    expect_error(el$is_selected(), "Element is not selectable")
+  }
 })
