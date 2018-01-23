4581b9857e63c2e5e5293da0bcfad8baaf7a3784
Gábor Csárdi <csardi.gabor@gmail.com>
2018-01-23 11:00:39 +0000

    Run the tests on the CIs

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,4 +7,6 @@ if (Sys.getenv("NOT_CRAN", "") != "" || Sys.getenv("CI", "") != "") {
   }
   if (is.null(webdriver:::find_phantom())) install_phantomjs()
   message("Using phantom.js from", webdriver:::find_phantom(), "\n")
+
+  test_check("webdriver")
 }
